a867adac5c279f642b08dc6b4013b4fff345e072
egorgasay <egorpoletaykin@gmail.com>
2022-08-21 00:18:26 +0300

    README

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # SQLTT  
 ## SQL Translator Tool 
   
-<img src="https://user-images.githubusercontent.com/102957432/181388596-621dead6-0f9c-4235-a46c-c348faf11c02.png" width="500" />
+<img src="https://user-images.githubusercontent.com/102957432/185766276-2b95b9da-d1cc-4f36-ab9e-a59018f61ea2.png" width="500" />
   
-❔ _Script for automatic translation of an sql query into another dialect written in Python._
+❔ _Script for automatic translation of an sql query into another dialect written in Python._  
+  
+  
+👀 First look  
+  
+![image](https://user-images.githubusercontent.com/102957432/185764567-a9c044bd-1257-47c8-8742-2019781df9b7.png)
--- forms.py
+++ forms.py
@@ -11,29 +11,29 @@ class CommonForm(FlaskForm):
         choices=list_db,
         render_kw={
             'class': 'form-control',
-            'style': 'width: auto'
+            'style': 'height: 80px; font-size: 34px'
         }
     )
     final_db = SelectField(
         'Выберите диалект на который нужно перевести',
         coerce=str,
-        choices=list_db,
+        choices=list_db[::-1],
         render_kw={
             'class': 'form-control',
-            'style': 'width: auto; padding-left: 100'
+            'style': 'height: 80px; font-size: 34px; '
         }
     )
 
     text = TextAreaField(label='Текст запроса',
                          render_kw={
                              'class': 'form-control',
-                             'style': 'width: 110%;',
+                             'style': 'height: 180px; width: 150%; ',
                          }
                          )
     submit = SubmitField("Перевести",
                          render_kw={
                              'class': 'form-control',
-                             'style': 'width: auto;'
+                             'style': 'height: 80px; width: 30%; font-size: 34px'
                          }
                          )
     # position = TextAreaField("HiddenField")
